2d6651a8597be9e96a659112da6847c292d0bd6c
vahid <naji.droid@gmail.com>
2019-09-07 15:22:31 +0430

    third

--- routers/router.go
+++ routers/router.go
@@ -8,7 +8,7 @@ import (
 
 func init() {
 	// beego.Router("/", &controllers.MainController{})
-	ns := beego.NewNamespace("/v1",
+	ns := beego.NewNamespace("https://app-fb9d7beb-1dd0-4872-b746-1ccf2a6f0e69.cleverapps.io/v1",
 		beego.NSNamespace("/user", beego.NSInclude(&controllers.UserController{})),
 	)
 	beego.AddNamespace(ns)
